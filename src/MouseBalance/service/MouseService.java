12ccbbc4c70e0b749e85ca3f38430df2929ece5b
Michael Strassberger <saremox@linux.com>
2014-06-14 05:05:29 +0200

    Extended MouseService with observer Pattern

--- src/MouseBalance/service/MouseService.java
+++ src/MouseBalance/service/MouseService.java
@@ -2,10 +2,11 @@ package MouseBalance.service;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Observable;
 
 import MouseBalance.materials.Mouse;
 
-public class MouseService
+public class MouseService extends Observable
 {
 	List<Mouse> _mouseList;
 	
@@ -17,5 +18,7 @@ public class MouseService
 	public void addMouse(Mouse sweetMouse)
 	{
 		_mouseList.add(sweetMouse);
+		this.setChanged();
+		this.notifyObservers();
 	}
 }
