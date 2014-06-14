b1040ab750a0d1e288cff0a84906b0eb463350b4
MadhouseKitty <3knickre@informatik.uni-hamburg.de>
2014-06-14 04:44:00 +0200

    Implemented MouseListModel

--- src/MouseBalance/tools/mouse/MouseListModel.java
+++ src/MouseBalance/tools/mouse/MouseListModel.java
@@ -1,8 +1,25 @@
 package MouseBalance.tools.mouse;
 
+import java.util.List;
+
 import javax.swing.AbstractListModel;
 
-public class MouseListModel extends AbstractListModel<E>
+import MouseBalance.materials.Mouse;
+
+public class MouseListModel extends AbstractListModel<Mouse>
 {
+	private List<Mouse> _mouse;
+	
+	@Override
+	public Mouse getElementAt(int row) 
+	{
+		return _mouse.get(row);
+	}
+
+	@Override
+	public int getSize() 
+	{
+		return _mouse.size();
+	}
 
 }
