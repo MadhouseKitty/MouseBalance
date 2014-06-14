6909bcfc3b721efd00863ea4c356bae75074bd6a
Michael Strassberger <saremox@linux.com>
2014-06-14 03:36:26 +0200

    Dialog and MeasurementTableModel prototypes

---
+++ src/MouseBalance/tools/MeasurementTableModel.java
@@ -0,0 +1,28 @@
+package MouseBalance.tools;
+
+import javax.swing.table.AbstractTableModel;
+
+public class MeasurementTableModel extends AbstractTableModel
+{
+	@Override
+	public int getColumnCount()
+	{
+		// TODO Auto-generated method stub
+		return 2;
+	}
+
+	@Override
+	public int getRowCount()
+	{
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Object getValueAt(int arg0, int arg1)
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
