44eb60b4c37d42d18cfc56dc5eedc63b9d2c9add
Saremox <saremox@linux.com>
2014-06-15 23:52:05 +0200

    Implemented MeasurementLoader Added a Notification interface for MouseService to inform the Loader to load Measurement Data for the Mouses

--- src/de/kitty/saremox/mousebalance/service/MeasurementService.java
+++ src/de/kitty/saremox/mousebalance/service/MeasurementService.java
@@ -8,6 +8,8 @@ import java.util.Observable;
 
 import de.kitty.saremox.mousebalance.materials.Measurement;
 import de.kitty.saremox.mousebalance.materials.Mouse;
+import de.kitty.saremox.mousebalance.service.io.MeasurementLoader;
+import de.kitty.saremox.mousebalance.service.io.MeasurementSaver;
 
 public class MeasurementService extends Observable {
 	Map<Mouse, List<Measurement>> _mousemap;
@@ -21,6 +23,7 @@ public class MeasurementService extends Observable {
 			_mousemap.put(sweetMouse, new ArrayList<Measurement>());
 		}
 		_mousemap.get(sweetMouse).add(measurement);
+		MeasurementSaver.saveMeasurement(sweetMouse, measurement);
 		setChanged();
 		notifyObservers();
 	}
@@ -30,5 +33,12 @@ public class MeasurementService extends Observable {
 			return new ArrayList<>(_mousemap.get(mice));
 		}
 		return new ArrayList<>();
+	}
+	
+	final void mouseLoadedEvent(Mouse mouse)
+	{
+		// A Mouse have been added due to a load
+		// We should now add Every measurement of the mice file
+		_mousemap.put(mouse, MeasurementLoader.loadMeasurements(mouse));
 	}
 }
--- src/de/kitty/saremox/mousebalance/service/io/MeasurementLoader.java
+++ src/de/kitty/saremox/mousebalance/service/io/MeasurementLoader.java
@@ -1,5 +1,56 @@
 package de.kitty.saremox.mousebalance.service.io;
 
-public class MeasurementLoader {
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
 
+import de.kitty.saremox.mousebalance.materials.Measurement;
+import de.kitty.saremox.mousebalance.materials.Mouse;
+import de.kitty.saremox.mousebalance.materials.Weight;
+
+public class MeasurementLoader {
+	public static List<Measurement> loadMeasurements(Mouse mouse)
+	{
+		ArrayList<Measurement> measurements = new ArrayList<>();
+		try(BufferedReader measurementReader = new BufferedReader(new FileReader(mouse.getName()+".mice")))
+		{
+			DateFormat format = new SimpleDateFormat();
+			String str;
+			while((str = measurementReader.readLine()) != null)
+			{
+				if(str.isEmpty())
+				{
+					continue;
+				}
+				
+				String[] measurementString = str.split("/");
+				try
+				{
+					measurements.add(new Measurement(new Weight(Integer.parseInt(measurementString[0])), format.parse(measurementString[1])));
+				}
+				catch(NumberFormatException e)
+				{
+					
+				}
+				catch(ParseException e)
+				{
+					
+				}
+			}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return measurements;
+	}
 }
