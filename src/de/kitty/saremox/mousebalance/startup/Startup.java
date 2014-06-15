7e43dbcf7114bfe8397105970f566e8e107d4a56
Saremox <saremox@linux.com>
2014-06-15 22:57:39 +0200

    Removed Example Mouses

--- src/de/kitty/saremox/mousebalance/startup/Startup.java
+++ src/de/kitty/saremox/mousebalance/startup/Startup.java
@@ -1,8 +1,5 @@
 package de.kitty.saremox.mousebalance.startup;
 
-import java.util.Date;
-
-import de.kitty.saremox.mousebalance.materials.Mouse;
 import de.kitty.saremox.mousebalance.tools.MainUi;
 import de.kitty.saremox.mousebalance.tools.measurement.MeasurementTool;
 import de.kitty.saremox.mousebalance.tools.mouse.MouseTool;
@@ -11,10 +8,6 @@ public class Startup {
 	public static void main(String[] args) {
 		MouseTool _mtool = new MouseTool();
 		MeasurementTool _measureTool = new MeasurementTool(_mtool);
-		_mtool.addMouse(new Mouse("Mocca", new Date(10000000000000l), "Brown"));
-
-		_mtool.addMouse(new Mouse("Mocca", new Date(10000000000000l), "Brown"));
 		new MainUi(_mtool.getUiPanel(), _measureTool.getUiPanel());
-
 	}
 }
